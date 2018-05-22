80bf77c1981fbd8e4b5065dd4a456476a598c785
Romain Quidet <contact@xdappfactory.com>
2018-05-22 10:05:18 +0200

    Initial Commit

---
+++ ChouetteMap/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ChouetteMap
+//
+//  Created by Romain Quidet on 22/05/2018.
+//  Copyright © 2018 Romain Quidet. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ ChouetteMap/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ChouetteMap
+//
+//  Created by Romain Quidet on 22/05/2018.
+//  Copyright © 2018 Romain Quidet. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
